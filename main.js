dbdd5c3bc6fcc49cacf892aa55c50882982747e0
김종현 <91518570+RevealOnTop@users.noreply.github.com>
2021-11-25 07:43:50 +0900

    lastest

--- main.js
+++ main.js
@@ -9,9 +9,20 @@ var app = http.createServer(function (request, response) {
   if (pathname === '/') {
     // undefined -> 정의되지 않았다는 뜻.
     if (queryData.id === undefined) {
+      fs.readdir('./', function(error, filelist){
+        console.log(filelist);
 
         var title = 'Welcome';
         var description = 'Hello, Node.js';
+        /*
+        let list = `<ul>
+        <li><a href="/?id=HTML">HTML</a></li>
+        <li><a href="/?id=CSS">CSS</a></li>
+        <li><a href="/?id=JavaScript">JavaScript</a></li>
+      </ul>`;
+      */
+
+      
         var template = `
           <!doctype html>
           <html>
@@ -21,11 +32,7 @@ var app = http.createServer(function (request, response) {
           </head>
           <body>
             <h1><a href="/">WEB</a></h1>
-            <ul>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>
+            
             <h2>${title}</h2>
             <p>${description}</p>
           </body>
@@ -33,6 +40,8 @@ var app = http.createServer(function (request, response) {
           `;
         response.writeHead(200);
         response.end(template);
+      })
+       
         
     } else {
       fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
---
+++ syntax/arr.js
@@ -0,0 +1,14 @@
+var arr = ['A','B','C','D'];
+console.log(arr[1]);
+console.log(arr[3]);
+
+// 배열 안의 값 변경하는 법
+arr[2] = 3;
+console.log(arr);
+
+// 배열의 길이 를 알아내는 법.
+console.log(arr.length);
+
+// 배열의 뒷자리에 변수혹은 내용 추가.(push)
+arr.push('E');
+console.log(arr);
